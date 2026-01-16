5cfb14b49d28a21d4d01c4e9e1c321bc33ed3de8
meatschool <ceojr1234@gmail.com>
2026-01-16 22:13:16 +0900

    refactor: prevent movement calculation when target is reached

--- Source/DungeonEscape/Mover.cpp
+++ Source/DungeonEscape/Mover.cpp
@@ -41,10 +41,15 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 
 	FVector CurrentLocation = GetOwner()->GetActorLocation();
 
-	float Speed = MoveOffset.Length() / MoveTime;
+	ReachedTarget = CurrentLocation.Equals(TargetLocation);
 
-	FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+	if (ReachedTarget == false)
+	{
+		float Speed = MoveOffset.Length() / MoveTime;
 
-	GetOwner()->SetActorLocation(NewLocation);
+		FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+
+		GetOwner()->SetActorLocation(NewLocation);
+	}
 }
 
