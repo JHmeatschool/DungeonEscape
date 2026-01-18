a3632efb0e72f940a230aff17af174255e098082
meatschool <ceojr1234@gmail.com>
2026-01-18 22:04:26 +0900

    feat: implement constructor for TriggerComponent and enable ticking

--- Source/DungeonEscape/TriggerComponent.cpp
+++ Source/DungeonEscape/TriggerComponent.cpp
@@ -3,14 +3,22 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Display, TEXT("Trriger component is alive!"));
+
 }
 
 void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+
 }
